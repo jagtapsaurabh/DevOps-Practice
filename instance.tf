d1c1effb1e44a63e01f9bfb189a715f4a9a447d5
jagtapsaurabh <jagtapsaurabh98@gmail.com>
2025-02-05 08:12:54 +0530

    this is new instance

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
   
 }
 resource "aws_instance" "myec2" {
-    ami = "ami-0b2506235506d0a0a"
+    ami = "ami-0678bf8844b66f72a"
     key_name = "demo"
     instance_type = "t2.micro"
     security_groups = ["sg"]
