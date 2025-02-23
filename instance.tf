986948a7c0f10cb478824b42d521061a5b4660d3
jagtapsaurabh <jagtapsaurabh98@gmail.com>
2025-02-23 09:56:38 +0530

    Marri Instance

--- instance.tf
+++ instance.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "myec2" {
     instance_type = "t2.micro"
     security_groups = ["sg"]
     tags = {
-      Nmae = "Spiderma_instance"
+      Nmae = "Marri_instance"
     }
   
 }
